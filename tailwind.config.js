0c23b8cdec209d8478292449adf57653580d4646
jahanrothy <jahanrothy58@gmail.com>
2024-06-02 00:32:39 +0600

    shop & view page complete

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+
+module.exports = {
+  content: ["index.html", "shop.html", "view.html"],
+  theme: {
+    screens:{
+      sm:"576px",
+      md:"768px",
+      lg:"992px",
+      xl:"1200px"
+  },
+  // container:{
+  //     center:true,
+  //     padding:"1rem"
+  // },
+
+    extend: {
+
+      fontFamily:{
+        BalsamiqSans:"'Balsamiq Sans' cursive",
+        roboto:"'Roboto', sans-serif" 
+      },
+      colors:{
+        "primary":"#166534"
+      }
+
+    },
+  },
+
+  variants:{
+    extend:{
+      display:["group-hover"],
+      visibility:["group-hover"],
+    
+},
+},
+
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
+
